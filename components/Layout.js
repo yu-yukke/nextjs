8ec691d3c010a142947892471ad9849f9eaa08e5
y.k <yukke5222@gmail.com>
2023-05-06 02:16:03 +0900

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import styles from '@/components/layout.module.css';
+import utilStyles from '@/styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'Hoge Fuga';
+export const siteTitle = 'Next.js Blog';
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${utilStyles.borderCircle}`}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && <Link href="/">←　ホームへ戻る</Link>}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+  return <h1>ページが見つかりません。</h1>;
+}
